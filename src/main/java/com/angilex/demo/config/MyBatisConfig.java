eab40c1a5f3960225c5676aaaba95164e8079dd1
hyisangie <angeline_shi@dlsu.edu.ph>
2022-01-09 18:07:54 +0800

    Mybatis配置类

---
+++ src/main/java/com/angilex/demo/config/MyBatisConfig.java
@@ -0,0 +1,16 @@
+/*
+ * 有了这个配置类就不用写@Mapper了
+ * 但我还是不用，这里只是记录一下，以后可以翻看
+ */
+
+/*package com.angilex.demo.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@MapperScan({"com.angilex.demo.mapper","com.angilex.demo.dao"})
+public class MyBatisConfig {
+}*/
